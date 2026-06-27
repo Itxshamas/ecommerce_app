094afb14ade68f2b5adc9e0e0964978b6ecbd267
Itxshamas <shamasulislam680@gmail.com>
2026-06-27 17:47:53 +0500

    Update product detail  page and improve UI components

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,20 +1,27 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
-const Productitem = ({id, image, name, price}) => {
-  
-    const {currency}= useContext(ShopContext)
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
 
   return (
-    <Link className='text-gray-700 cursor-pointer ' to={`/produt/${id}`}>
-      <div className='overflow-hidden'>
-      <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt=' '/>
+    <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer block">
+      <div className="overflow-hidden">
+        <img
+          className="hover:scale-110 transition ease-in-out duration-300 w-full"
+          src={image?.[0]}
+          alt={name}
+        />
       </div>
-      <p className='pt-3 pb-1 text-sm'>{name}</p>
-      <p className='text-sm font-medium'>{currency}{price}</p>
-    </Link>
-  )
-}
 
-export default Productitem
+      <p className="pt-3 pb-1 text-sm">{name}</p>
+      <p className="text-sm font-medium">
+        {currency}
+        {price}
+      </p>
+    </Link>
+  );
+};
+
+export default ProductItem;
